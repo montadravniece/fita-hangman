077515cb419fea360e263f8847b37d004c082256
Monta Dravniece <montadravniece@gmail.com>
2021-10-09 13:13:06 +0300

    pec 2 uzdevuma vairs neko nesaprotu "nezinu pat datumu"

--- gameplay/game.py
+++ gameplay/game.py
@@ -1,85 +1,85 @@
-import random
+class Game:
+    def __init__(self, word):
+        self.word = word.upper()
+        self.word_by_letters = list(self.word)
+        self.length_of_word = len(self.word)
+        
+        self.lives = 6
+        self.word_is_guessed = False
+        
+        self.empty_word = list(len(self.word) * "-")
+        self.guessed_letters = list()
+        self.missed_letters = list()
+        self.missed_words = list()
+        
+        
+        
+    def play (self):
+        user_name = str(input("Hello user. Please, enter your name!"))
+        print("Hello, ", user_name)
 
-user_name = str(input("Hello user. Please, enter your name!"))
-print("Hello, ", user_name)
+        while True:
+            while self.lives != 0 and self.word_is_guessed == False:
+                guess = input('Please type your letter: ').upper()
+                length_of_guess = len(guess)
+                # check lenght and if guess is aplhabetic
+                if length_of_guess == 1 and guess.isalpha():
+                    if guess in self.guessed_letters: # Checking if letter is already guessed    
+                        print("You have already tried this letter")
 
-# game variables
-
-words= 'LIETUS', "ANDROID", "VASARA"
-word = random.choice(words)
-length_of_word = len(word)
-word_by_letters = list(word)
-empty_word = list(length_of_word * "-")
-
-lives = 6
-word_is_guessed = False
-guessed_letters = list()
-guessed_word_set = set(word)
-missed_letters = list()
-missed_words = list()
-
-# game code
-while True:
-    while lives != 0 and word_is_guessed == False:
-        guess = input('Please type your letter: ').upper()
-        length_of_guess = len(guess)
-        # check lenght and if guess is aplhabetic
-        if length_of_guess == 1 and guess.isalpha():
-            if guess in guessed_letters: # Checking if letter is already guessed    
-                print("You have already tried this letter")
-
-            if guess in word_by_letters:
-                # dzīvības nemainas
-                guessed_letters.append(guess) # Add guessed letter to guessed letters list
-                ########################################
-                for i, j in enumerate(word_by_letters): 
-                    if j == guess:
-                        empty_word[i] = guess # 'Open' correctly guessed letter    
-                #########################################
-                print('Correct!', empty_word, '\n', 'Guess: ', guessed_letters, '\n', 'Missed: ', missed_letters, '\n', 'Missed words: ', missed_words, '\n', 'Lives: ', lives)
-                if empty_word == word_by_letters: # Check if word is guessed
-                    word_is_guessed = True
-                    break 
+                    if guess in self.word_by_letters:
+                        # dzīvības nemainas
+                        self.guessed_letters.append(guess) # Add guessed letter to guessed letters list
+                        ########################################
+                        for i, j in enumerate(self.word_by_letters): 
+                            if j == guess:
+                                self.empty_word[i] = guess # 'Open' correctly guessed letter    
+                        #########################################
+                        print('Correct!', self.empty_word, '\n', 'Guess: ', self.guessed_letters, '\n', 'Missed: ', self.missed_letters, '\n', 'Missed words: ', self.missed_words, '\n', 'Lives: ', self.lives)
+                        if self.empty_word == self.word_by_letters: # Check if word is guessed
+                            self.word_is_guessed = True
+                            break 
+                    else:
+                        # dzīvības mīnusā
+                        if guess in self.missed_letters:
+                            print("You have already tried this letter")
+                        else:
+                            self.lives = self.lives - 1
+                            self.missed_letters.append(guess)
+                            print('Wrong!', self.empty_word, '\n', 'Guess: ', self.guessed_letters, '\n', 'Missed: ', self.missed_letters, '\n', 'Missed words: ', self.missed_words, '\n', 'Lives: ', self.lives)
+                else:
+                    if guess in self.missed_words:
+                            print("You have already tried this word")
+                    elif length_of_guess == self.length_of_word and guess.isalpha():
+                        if guess == self.word:
+                            self.word_is_guessed = True
+                            self.empty_word = self.word
+                            break
+                        else:
+                            self.missed_words.append(guess)
+                            self.lives = self.lives - 1
+                            print('Wrong!', self.empty_word, '\n', 'Guess: ', self.guessed_letters, '\n', 'Missed: ', self.missed_letters, '\n', 'Missed words: ', self.missed_words, '\n', 'Lives: ', self.lives)
+                    else:
+                        if length_of_guess > self.length_of_word and guess.isalpha():
+                            print("Sorry, ", user_name, "the word you entered is too long!")
+                            print(self.empty_word, '\n', 'Guess: ', self.guessed_letters, '\n', 'Missed: ', self.missed_letters, '\n', 'Missed words: ', self.missed_words, '\n', 'Lives: ', self.lives)
+                        elif length_of_guess < self.length_of_word and guess.isalpha():
+                            print("Sorry, ", user_name, "the word you entered is too short!")
+                            print(self.empty_word, '\n', 'Guess: ', self.guessed_letters, '\n', 'Missed: ', self.missed_letters, '\n', 'Missed words: ', self.missed_words, '\n', 'Lives: ', self.lives)
+                        else:
+                            print("Wrong input type!")
+                            print(self.empty_word, '\n', 'Guess: ', self.guessed_letters, '\n', 'Missed: ', self.missed_letters, '\n', 'Missed words: ', self.missed_words, '\n', 'Lives: ', self.lives)
+            if self.word_is_guessed == True:
+                print("Congratulations! You won! The correct word was:", self.word, "!")
+            elif self.lives == 0:
+                print("Sorry! Game over! Correct word was", self.word, '!')
             else:
-                # dzīvības mīnusā
-                if guess in missed_letters:
-                    print("You have already tried this letter")
-                else:
-                    lives = lives - 1
-                    missed_letters.append(guess)
-                    print('Wrong!', empty_word, '\n', 'Guess: ', guessed_letters, '\n', 'Missed: ', missed_letters, '\n', 'Missed words: ', missed_words, '\n', 'Lives: ', lives)
-        else:
-            if guess in missed_words:
-                    print("You have already tried this word")
-            elif length_of_guess == length_of_word and guess.isalpha():
-                if guess == word:
-                    word_is_guessed = True
-                    empty_word = word
-                    break
-                else:
-                    missed_words.append(guess)
-                    lives = lives - 1
-                    print('Wrong!', empty_word, '\n', 'Guess: ', guessed_letters, '\n', 'Missed: ', missed_letters, '\n', 'Missed words: ', missed_words, '\n', 'Lives: ', lives)
+                print("What just happend?")
+
+
+            exit_ = str(input('do you want to exit? y/n'))
+            if exit_ == 'y':
+                break
             else:
-                if length_of_guess > length_of_word and guess.isalpha():
-                    print("Sorry, ", user_name, "the word you entered is too long!")
-                    print(empty_word, '\n', 'Guess: ', guessed_letters, '\n', 'Missed: ', missed_letters, '\n', 'Missed words: ', missed_words, '\n', 'Lives: ', lives)
-                elif length_of_guess < length_of_word and guess.isalpha():
-                    print("Sorry, ", user_name, "the word you entered is too short!")
-                    print(empty_word, '\n', 'Guess: ', guessed_letters, '\n', 'Missed: ', missed_letters, '\n', 'Missed words: ', missed_words, '\n', 'Lives: ', lives)
-                else:
-                    print("Wrong input type!")
-                    print(empty_word, '\n', 'Guess: ', guessed_letters, '\n', 'Missed: ', missed_letters, '\n', 'Missed words: ', missed_words, '\n', 'Lives: ', lives)
-    if word_is_guessed == True:
-        print("Congratulations! You won! The correct word was:", word, "!")
-    elif lives == 0:
-        print("Sorry! Game over! Correct word was", word, '!')
-    else:
-        print("What just happend?")
-
-
-    exit_ = str(input('do you want to exit? y/n'))
-    if exit_ == 'y':
-      break
-    else:
-        continue
+                continue
+        
---
+++ hangman.py
@@ -0,0 +1,4 @@
+from gameplay.game import Game
+
+game = Game('lopuhs')
+game.play() 
