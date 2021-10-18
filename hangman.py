2b25f353720c25cf0b8cb409764ae1312f313454
Monta Dravniece <montadravniece@gmail.com>
2021-10-18 20:22:43 +0300

    tika izpildīts 5.uzdevums

--- hangman.py
+++ hangman.py
@@ -1,12 +1,28 @@
 from gameplay.game import Game
-import random
+import random 
+from nltk import word_tokenize
 
-words = 'seja', 'zakis', 'ziepes'
 
+with open('data/words.txt', 'r', encoding='utf-8') as file:
+    words = file.read()
+    words = word_tokenize(words)
+    # words = list(words.split("\n"))
+    # random.choice(word)
+    # word = list(word)
+    # word.pop()
+    #words = file.read()
+    #words = word_tokenize(words)
+    #print(words)
+
+# Randomizē spēles palaišanas brīdī (1x) - pietiekami
+#random.shuffle(words)
 
 while True:
-
-    game = Game(random.choice(words))
+    # Randomizē katrā spēlē
+    random.shuffle(words)
+    word = words.pop()
+    game = Game(word)
+    #game = Game(random.choice(words))
     game.play()
     
     exit_ = str(input('do you want to exit? y/n: '))
